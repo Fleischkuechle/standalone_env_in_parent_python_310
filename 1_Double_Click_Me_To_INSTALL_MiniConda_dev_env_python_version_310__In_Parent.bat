e90c55d62a4fd8085f2ee2ab1124d435712863bc
Fleischkuechle <zimmkon@yahoo.de>
2024-12-18 01:44:29 +0100

    checksum update to latest miniconda version

--- 1_Double_Click_Me_To_INSTALL_MiniConda_dev_env_python_version_310__In_Parent.bat
+++ 1_Double_Click_Me_To_INSTALL_MiniConda_dev_env_python_version_310__In_Parent.bat
@@ -38,8 +38,8 @@ set INSTALL_ENV_DIR=%cd%\dev_env\env
 @REM set MINICONDA_CHECKSUM=307194e1f12bbeb52b083634e89cc67db4f7980bd542254b43d3309eaf7cb358
 @REM chaged to miniconda latest.
 set MINICONDA_DOWNLOAD_URL=https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe
-set MINICONDA_CHECKSUM=3a8897cc5d27236ade8659f0e119f3a3ccaad68a45de45bfdd3102d8bec412ab
-
+@REM set MINICONDA_CHECKSUM=3a8897cc5d27236ade8659f0e119f3a3ccaad68a45de45bfdd3102d8bec412ab
+set MINICONDA_CHECKSUM=be382fc02742c734fd3c399c5e5d322bc9b43292117827ab9aa6f7446351e45c
 
 set conda_exists=F
 
