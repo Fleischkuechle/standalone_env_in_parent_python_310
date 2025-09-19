6ff85430618f71d178c290b6b7f3bd5daf681829
Fleischkuechle <zimmkon@yahoo.de>
2025-09-19 16:14:26 +0200

    added 1_2_Double_Click_Me_To_INSTALL_Miniconda3-py310_25.7.0-2-Windows-x86_64_

---
+++ 1_2_Double_Click_Me_To_INSTALL_Miniconda3-py310_25.7.0-2-Windows-x86_64_.bat
@@ -0,0 +1,103 @@
+@echo off
+setlocal enabledelayedexpansion
+@REM %~dp0: Breaks down as follows:
+@REM %~d0: Extracts the drive letter from %0.
+@REM %~p0: Extracts the path (directory) from %0.
+@REM Combining the above two (%~d0 and %~p0) gives the drive and path of the batch file.
+
+@REM REM this is getting the path where the bat file is.
+@REM cd /D "%~dp0"
+
+REM this is getting the parent path fro where the bat file is.
+cd /D "%~dp0.."
+
+set PATH=%PATH%;%SystemRoot%\system32
+
+echo "%CD%"| findstr /C:" " >nul && echo This script relies on Miniconda which can not be silently installed under a path with spaces. && goto end
+
+@rem Check for special characters in installation path
+set "SPCHARMESSAGE="WARNING: Special characters were detected in the installation path!" "         This can cause the installation to fail!""
+echo "%CD%"| findstr /R /C:"[!#\$%&()\*+,;<=>?@\[\]\^`{|}~]" >nul && (
+	call :PrintBigMessage %SPCHARMESSAGE%
+)
+set SPCHARMESSAGE=
+
+@rem fix failed install when installing to a separate drive
+set TMP=%cd%\dev_env
+set TEMP=%cd%\dev_env
+
+@rem deactivate existing conda envs as needed to avoid conflicts
+(call conda deactivate && call conda deactivate && call conda deactivate) 2>nul
+
+@rem config
+set INSTALL_DIR=%cd%\dev_env
+set CONDA_ROOT_PREFIX=%cd%\dev_env\conda
+set INSTALL_ENV_DIR=%cd%\dev_env\env
+@REM see here for other versions: https://repo.anaconda.com/miniconda/
+set MINICONDA_DOWNLOAD_URL=https://repo.anaconda.com/miniconda/Miniconda3-py310_25.7.0-2-Windows-x86_64.exe
+set MINICONDA_CHECKSUM=3f5b24e9873bf12a72b7a639e7d59d3cc1015f4cd552dbaf981cc0518be271ed
+set conda_exists=F
+
+@rem figure out whether git and conda needs to be installed
+call "%CONDA_ROOT_PREFIX%\_conda.exe" --version >nul 2>&1
+if "%ERRORLEVEL%" EQU "0" set conda_exists=T
+
+@rem (if necessary) install git and conda into a contained environment
+@rem download conda
+if "%conda_exists%" == "F" (
+	echo Downloading Miniconda from %MINICONDA_DOWNLOAD_URL% to %INSTALL_DIR%\miniconda_installer.exe
+
+	mkdir "%INSTALL_DIR%"
+	call curl -Lk "%MINICONDA_DOWNLOAD_URL%" > "%INSTALL_DIR%\miniconda_installer.exe" || ( echo. && echo Miniconda failed to download. && goto end )
+
+	for /f %%a in ('CertUtil -hashfile "%INSTALL_DIR%\miniconda_installer.exe" SHA256 ^| find /i /v " " ^| find /i "%MINICONDA_CHECKSUM%"') do (
+		set "output=%%a"
+	)
+
+	if not defined output (
+		echo The checksum verification for miniconda_installer.exe has failed.
+		del "%INSTALL_DIR%\miniconda_installer.exe"
+		goto end
+	) else (
+		echo The checksum verification for miniconda_installer.exe has passed successfully.
+	)
+
+	echo Installing Miniconda to %CONDA_ROOT_PREFIX%
+	start /wait "" "%INSTALL_DIR%\miniconda_installer.exe" /InstallationType=JustMe /NoShortcuts=1 /AddToPath=0 /RegisterPython=0 /NoRegistry=1 /S /D=%CONDA_ROOT_PREFIX%
+
+	@rem test the conda binary
+	echo Miniconda version:
+	call "%CONDA_ROOT_PREFIX%\_conda.exe" --version || ( echo. && echo Miniconda not found. && goto end )
+
+	@rem delete the Miniconda installer
+	del "%INSTALL_DIR%\miniconda_installer.exe"
+)
+
+@rem create the installer env
+if not exist "%INSTALL_ENV_DIR%" (
+	echo Packages to install: %PACKAGES_TO_INSTALL%
+	call "%CONDA_ROOT_PREFIX%\_conda.exe" create --no-shortcuts -y -k --prefix "%INSTALL_ENV_DIR%" python=3.10 || ( echo. && echo Conda environment creation failed. && goto end )
+)
+
+@rem check if conda environment was actually created
+if not exist "%INSTALL_ENV_DIR%\python.exe" ( echo. && echo Conda environment is empty. && goto end )
+
+@rem environment isolation
+set PYTHONNOUSERSITE=1
+set PYTHONPATH=
+set PYTHONHOME=
+set "CUDA_PATH=%INSTALL_ENV_DIR%"
+set "CUDA_HOME=%CUDA_PATH%"
+
+REM ".\dev_env\conda\condabin\conda.bat"  activate "\dev_env\env"
+@rem activate installer env
+call "%CONDA_ROOT_PREFIX%\condabin\conda.bat" activate "%INSTALL_ENV_DIR%" || ( echo. && echo Miniconda hook not found. && goto end )
+
+@REM @rem setup installer env
+@REM call python reqirement_installer.py %*
+
+@rem below are functions for the script   next line skips these during normal execution
+goto end
+
+:end
+pause
